127a79672a12324e0bc1daf823f799faacd08d7d
e <2475980354@qq.com>
2019-05-10 08:12:58 +0800

    第一个页面

--- app.js
+++ app.js
@@ -37,8 +37,3 @@ App({
     userInfo: null
   }
 })
-const config = {
-  api_base_url: 'https://www.koocv.com/',
-  // appKey: '5ZbxAY1FmDNQP1T1'
-}
-export { config }
---
+++ childs/homes/home-3/home-3.js
@@ -0,0 +1,23 @@
+// childs/homes/home-3/home-3.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
